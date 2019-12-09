0c70d0778f0ab4d50b9919a96dc1d4a3432260a1
pedroths95 <58697039+pedroths95@users.noreply.github.com>
2019-12-09 15:45:30 -0300

    EX.py now does gaussian elimination
    -> Updated inverse and transpose functions
    -> Need to build better matrix maker functions: Maybe a class?
    -> Need to fix code (docstrings/comments)
    -> Branch Complete

--- EX.py
+++ EX.py
@@ -2,9 +2,12 @@ import sys
 from random import seed
 from random import randint
 from random import random
-import numpy
+import numpy as np
 from numpy import copy
 from numpy.linalg import inv
+from numpy.linalg import solve
+from numpy.linalg import det
+import copy
 seed(7)
 matrix=[]
 n=7
@@ -13,7 +16,13 @@ def make_square_matrix(n): # n rows m colunms matrix
     Row: Plus 1
     Col: n times 0.125
     """
-    A=[[1+0.125*j for i in np.linspace(0.125,8)] for j in np.linspace(0.125,8)]
+    A=[]
+    for i in range(n):
+        row=[]
+        for j in range(n):
+            f=randint(0,n)
+            row.append(f)
+        A.append(row)
     return A
 def test_matrix(A,name): # Compare A with A transpose
     """ Count how many rows are equal in A and A transpose
@@ -59,6 +68,24 @@ def matrix_mult(A,B):
             for k in range(cA): # k range in A colunms
                 C[i][j]+=A[i][k]*B[k][j]
     return C
+def copy_matrix(A):
+    """    A: matrix to be copied
+    Returns :  A copy of A
+    """
+    A_copy=[[]]
+    for row in A:
+        for col in (A[0]):
+            A_copy[i][j]=A[i][j]
+    return A_copy
+#def gauss_elim(A):
+#    for row in range(n): # Search for maximum in row
+#        #k=i+1
+#        max_el=np.abs(A[row])
+#        for col in range(n):
+#            if A[row][col] > max_el:
+#                max_el=A[k][row]
+#                max_row=k
+
 
 def determinant(A,t=0):
     """ Calculate a determinant recursively
@@ -71,7 +98,6 @@ def determinant(A,t=0):
     sign      : Depends on the index, (even > 0, odd < 0)
     sub_det   : Recursive determinant of sub_matrix
     """
-
     index=list(range(len(A)))
     # 2X2 Matrix Case
     if len(A) == 2 and len(A[0]) == 2:
@@ -80,15 +106,76 @@ def determinant(A,t=0):
     for row in index:
         sub_matrix=A[1:] # Cut the first row
         N_cols=len(sub_matrix)
-        print(N_cols)
-        print(sub_matrix,' Submatrix of N_cols = ',N_cols)
+       # print(N_cols)
+       # print(sub_matrix,' Submatrix of N_cols = ',N_cols)
         for col in range(N_cols): # loop on colunms
             sub_matrix[col]=sub_matrix[col][0:row]+sub_matrix[col][row+1:] # Remove colunm
         sign=(-1)**(row%2) # sub_det is None?
         sub_det=determinant(sub_matrix)
         t+=sign*A[0][row]*sub_det 
     return t
+def transpose_matrix(A):
+    A_t=[]
+    for i in range(n):
+        row=[]
+        for j in range(n):
+            row.append(A[j][i])
+        A_t.append(row)
+    return A_t
+        
 
+def get_minor(A,i,j):
+    return [row[:j] + row[j+1:] for row in (A[:i]+A[i+1:])]
+def get_inverse(A):
+    d=determinant(A)
+    if len(A) == 2:
+        return [[A[1][1]/d, -1*A[0][1]/d],
+                [-1*A[1][0]/d, A[0][0]/d]]
+    cofactors=[]
+    for r in range(len(A)):
+        cofactorRow=[]
+        for c in range(len(A)):
+            minor=get_minor(A,r,c)
+            cofactorRow.append(((-1)**(r+c))*determinant(minor))
+        cofactors.append(cofactorRow)
+    cofactors=transpose_matrix(cofactors)
+    for r in range(len(cofactors)):
+        for c in range(len(cofactors)):
+            cofactors[r][c]=cofactors[r][c]/d
+    return cofactors
+def gauss(a, b):
+    a = copy.deepcopy(a)
+    b = copy.deepcopy(b)
+    n = len(a)
+    p = len(b[0])
+    det = 1
+    for i in range(n - 1):
+        k = i
+        for j in range(i + 1, n):
+            if abs(a[j][i]) > abs(a[k][i]):
+                k = j
+        if k != i:
+            a[i], a[k] = a[k], a[i]
+            b[i], b[k] = b[k], b[i]
+            det = -det
+
+        for j in range(i + 1, n):
+            t = a[j][i]/a[i][i]
+            for k in range(i + 1, n):
+                a[j][k] -= t*a[i][k]
+            for k in range(p):
+                b[j][k] -= t*b[i][k]
+
+    for i in range(n - 1, -1, -1):
+        for j in range(i + 1, n):
+            t = a[i][j]
+            for k in range(p):
+                b[i][k] -= t*b[j][k]
+        t = 1/a[i][i]
+        det *= a[i][i]
+        for j in range(p):
+            b[i][j] *= t
+    return det, b
 
 
 
@@ -96,15 +183,24 @@ def determinant(A,t=0):
 A=make_square_matrix(n)
 #test_matrix(A,'A') # Matrix A is being tested
 # List comprehension to make transpose matrix
-A_t=[[A[j][i] 
-    for j in range(len(A))]
-    for i in range(len(A[0]))]
-print(determinant(A))
-
-
+#A_t=[[A[j][i] 
+#    for j in range(len(A))]
+#    for i in range(len(A[0]))]
+print(determinant(A),' Calculated determinant')
+print(det(A),' Numpy determinant')
+A_t=transpose_matrix(A)
 # Answers below
-#print_matrix(A,'A')
-#print_matrix(A_t,'A transpose')
-#RESP_A=matrix_mult(A,A_t)
-#print_matrix(RESP_A,'Answer A')
+print_matrix(A,'A')
+print_matrix(A_t,'A transpose')
+RESP_A=matrix_mult(A,A_t)
+print_matrix(RESP_A,'Answer A: A times A transpose = ')
+RESP_B=get_inverse(A)
+print_matrix(RESP_B,'Answer B: Inverse of A is = ') 
+seed(23)
+B=make_square_matrix(n)
+det_C,x=gauss(A,B)
+print(' Values of x = ',x)
+print(' Determinant = ',det_C)
+print(' Numpy solution =',solve(A,B))
+
 
